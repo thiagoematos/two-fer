ad7d300875d6bf1f385d731d41b876726cf7ee5a
Thiago Matos <thiago.encarnacaomatos-ext@ab-inbev.com>
2024-02-01 19:10:33 -0300

    data driven test

--- src/test/kotlin/br/bees/twofer/TwoFerUnitTest.kt
+++ src/test/kotlin/br/bees/twofer/TwoFerUnitTest.kt
@@ -2,47 +2,21 @@ package br.bees.twofer
 
 import br.bees.twofer.domain.TwoFer
 import io.kotest.core.spec.style.ShouldSpec
+import io.kotest.data.forAll
+import io.kotest.data.row
 import io.kotest.matchers.shouldBe
 
 class TwoFerUnitTest : ShouldSpec({
-    should("return message 'One for Bob, one for me' when for name is 'Bob'") {
-        // given
-        val nome = "Bob"
-
-        // when
-        val message = TwoFer().message(nome)
-
-        // then
-        message shouldBe "One for Bob, one for me"
-    }
-    should("return message 'One for Alice, one for me' when for name is 'Alice'") {
-        // given
-        val nome = "Alice"
-
-        // when
-        val message = TwoFer().message(nome)
-
-        // then
-        message shouldBe "One for Alice, one for me"
-    }
-    should("return message 'One for you, one for me' when for name is not provided") {
-        // given
-        val nome = ""
-
-        // when
-        val message = TwoFer().message(nome)
-
-        // then
-        message shouldBe "One for you, one for me"
-    }
-    should("return message 'One for Zaphod, one for me' when for name is 'Zaphod'") {
-        // given
-        val nome = "Zaphod"
-
-        // when
-        val message = TwoFer().message(nome)
-
-        // then
-        message shouldBe "One for Zaphod, one for me"
+    context("TwoFer Unit Test") {
+        forAll(
+            row("Bob", "One for Bob, one for me"),
+            row("Alice", "One for Alice, one for me"),
+            row("Zaphod", "One for Zaphod, one for me"),
+            row("", "One for you, one for me")
+        ) { name, expected ->
+            should("return message '$expected' when for name is '$name'") {
+                TwoFer().message(name) shouldBe expected
+            }
+        }
     }
 })
